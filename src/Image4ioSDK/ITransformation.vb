7bbb150960be6abc5c381f6ce842a55c05a7f264
jackkoolage <59254387+jackkoolage@users.noreply.github.com>
2020-03-27 12:07:31 +0200

    Delete ITransformation.vb

--- src/Image4ioSDK/ITransformation.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Interface ITransformation
-    Function ToWebP(Quality As Integer, Optional Width As Integer? = Nothing, Optional Height As Integer? = Nothing) As String
-    Function Resize(Quality As Integer, Optional Width As Integer? = Nothing, Optional Height As Integer? = Nothing) As String
-    Function Compress(Quality As Integer) As String
-
-End Interface
